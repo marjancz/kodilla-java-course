87a3b198d14abdddfdd2e0b1690c28f3f84c86ca
marjancz <marriusz@janczyk.com.pl>
2018-01-14 21:27:07 +0100

    work6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,6 +1,4 @@
 package com.kodilla.testing;
-import com.kodilla.testing.user.SimpleUser;
-import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
