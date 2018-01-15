0b2f277d81f306635d61e1d8c5f050de80ba46ad
marjancz <marriusz@janczyk.com.pl>
2018-01-15 18:14:23 +0100

    mod6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain62.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain62.java
@@ -3,24 +3,16 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain62 {
     public static void main(String[] args) {
-        Calculator cal = new Calculator(345, 467);
+        Calculator cal = new Calculator(655, 345);
         int sum = cal.sum();
         int subs = cal.substract();
         int a = cal.getA();
         int b = cal.getB();
-        int rightA = 1000 - b;
-        int rightB = 1000 - a;
 
-        if (sum < 1000) {
-            System.out.println("Test of sum is OK.!");
+        if ((sum == 1000) && (subs == 310)) {
+            System.out.println("Test of calculator is OK.!");
         } else {
-            System.out.println("Error!!! Set \"a\" higher than: [" + rightA + "] or set \"b\" higher than: [" + rightB + "]");
-        }
-
-        if (subs > 0) {
-            System.out.println("Test of substract is OK.!");
-        } else {
-            System.out.println("Error!!! Set \"b\" lower than: [" + a + "]");
+            System.out.println("Error!!!");
         }
     }
 }
