4a218525177b100b2b843ddc7526804ee9e05226
marjancz <marriusz@janczyk.com.pl>
2018-02-20 04:01:15 +0100

    work 10.1

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -1,4 +1,13 @@
 package com.kodilla.spring.forum;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ForumUser {
+    String username = "John Smith";
+
+    public String ForumUser(String username) {
+        return username;
+    }
 }
+
