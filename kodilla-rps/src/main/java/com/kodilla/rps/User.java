39c3cfade8cc0775cf19c86154528009ca5a6d98
“marjancz <mariusz@janczyk.com.pl>
2018-02-25 03:58:27 +0100

    rps

--- kodilla-rps/src/main/java/com/kodilla/rps/User.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/User.java
@@ -1,4 +1,28 @@
 package com.kodilla.rps;
 
+import java.util.Scanner;
+
 public class User {
+    public String getName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please type your name:" +
+                "\n(minimum 3 characters)" +
+                "\n(maximum 8 characters)");
+        String user = scanner.next();
+        if(user.length() < 3) {
+            System.out.println("Too short name!" +
+                    "\nPlease type your name again:" +
+                    "\n(minimum 3 characters)" +
+                    "\n(maximum 8 characters)");
+            user = scanner.next();
+        }
+        if(user.length() > 8) {
+            System.out.println("Too long name!" +
+                    "\nPlease type your name again:" +
+                    "\n(minimum 3 characters)" +
+                    "\n(maximum 8 characters)");
+            user = scanner.next();
+        }
+        return user;
+    }
 }
