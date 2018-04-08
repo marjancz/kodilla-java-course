465b34ea93da3a71144ed528236d795da4848927
“marjancz <mariusz@janczyk.com.pl>
2018-04-08 07:36:10 +0200

    work 13.3

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,52 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    public void testInvoiceDoaSave() {
+        //Given
+        Product product1 = new Product("Hammer");
+        Product product2 = new Product("Screw driver");
+        Product product3 = new Product("Spade");
+
+        Item item1 = new Item(product1, new BigDecimal(12.99),
+                3, new BigDecimal(38.97));
+        Item item2 = new Item(product2, new BigDecimal(9.5),
+                5, new BigDecimal(47.50));
+        Item item3 = new Item(product3, new BigDecimal(25.00),
+                3, new BigDecimal(75.00));
+
+        Invoice invoice = new Invoice("001/2018");
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+        item3.setInvoice(invoice);
+
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+        invoice.getItems().add(item3);
+
+        //When
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        //Then
+        Assert.assertEquals(1, id);
+
+        //CleanUp
+        invoiceDao.delete(id);
+    }
+}
